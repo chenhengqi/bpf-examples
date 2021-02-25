072f723b163d8c6a3f72ba7ff6e90f0618b8652d
chenhengqi <chenhengqi@outlook.com>
2021-02-25 09:01:04 +0800

    update sample program
    Signed-off-by: chenhengqi <chenhengqi@outlook.com>

--- uretprobe-with-golang-the-wrong-way/tracee/main.go
+++ uretprobe-with-golang-the-wrong-way/tracee/main.go
@@ -7,18 +7,33 @@ import (
 )
 
 //go:noinline
-func foobar(s string) (int, int) {
-	fmt.Println(s)
-	var arr [1024 * 16]byte
-	for i, b := range []byte(s) {
-		arr[i] = b
-	}
-	return len(s), len(arr)
+func foobar() (int, string, int) {
+	var x [10]int
+	foo(x)
+	return 0xbeef, os.Args[1], 0xfaac
+}
+
+//go:noinline
+func foo(x [10]int) {
+	var y [100]int
+	bar(y)
+}
+
+//go:noinline
+func bar(y [100]int) {
+	var z [1000]int
+	buz(z)
+}
+
+//go:noinline
+func buz(z [1000]int) {
+
 }
 
 func main() {
 	for {
-		foobar(os.Args[1])
+		a, b, c := foobar()
+		fmt.Printf("%x %s %x\n", a, b, c)
 		time.Sleep(time.Second)
 	}
 }
