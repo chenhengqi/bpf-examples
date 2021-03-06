ea36cb78f5527f0524eb491053b467f8226b3bd3
chenhengqi <chenhengqi@outlook.com>
2021-03-06 16:52:07 +0800

    add tcp_drop log

--- trace-tcpreset-and-tcpretransmit/tracer/doc.go
+++ trace-tcpreset-and-tcpretransmit/tracer/doc.go
@@ -1,3 +1,4 @@
 package main
 
 // http://www.brendangregg.com/blog/2018-03-22/tcp-tracepoints.html
+// http://www.brendangregg.com/blog/2018-05-31/linux-tcpdrop.html
--- trace-tcpreset-and-tcpretransmit/tracer/main.go
+++ trace-tcpreset-and-tcpretransmit/tracer/main.go
@@ -25,13 +25,23 @@ int log_tcp_reset(struct pt_regs *ctx, struct sock *sk) {
     bpf_trace_printk("tcp_reset port=%d dst=%d state=%d\n", port, daddr, state);
     return 0;
 }
+
+int log_tcp_drop(struct pt_regs *ctx, struct sock *sk) {
+    u16 port = sk->__sk_common.skc_dport;
+    u32 daddr = sk->__sk_common.skc_daddr;
+    u8 state = sk->__sk_common.skc_state;
+    bpf_trace_printk("tcp_drop port=%d dst=%d state=%d\n", port, daddr, state);
+    return 0;
+}
 `
 
 const (
 	funcNameTCPRetransmit  = "log_tcp_retransmit"
 	funcNameTCPReset       = "log_tcp_reset"
+	funcNameTCPDrop        = "log_tcp_drop"
 	eventNameTCPRetransmit = "tcp_retransmit_skb"
 	eventNameTCPReset      = "tcp_reset"
+	eventNameTCPDrop       = "tcp_drop"
 	maxActive              = 0
 )
 
@@ -39,22 +49,32 @@ func main() {
 	bpfModule := bcc.NewModule(bpfProgram, []string{})
 	defer bpfModule.Close()
 
+	// TCP Retransmission
 	uprobeFD, err := bpfModule.LoadKprobe(funcNameTCPRetransmit)
 	if err != nil {
 		panic(err)
 	}
-
 	err = bpfModule.AttachKprobe(eventNameTCPRetransmit, uprobeFD, maxActive)
 	if err != nil {
 		panic(err)
 	}
 
+	// TCP Reset
 	uprobeFD, err = bpfModule.LoadKprobe(funcNameTCPReset)
 	if err != nil {
 		panic(err)
 	}
-
 	err = bpfModule.AttachKprobe(eventNameTCPReset, uprobeFD, maxActive)
+	if err != nil {
+		panic(err)
+	}
+
+	// TCP Drop
+	uprobeFD, err = bpfModule.LoadKprobe(funcNameTCPDrop)
+	if err != nil {
+		panic(err)
+	}
+	err = bpfModule.AttachKprobe(eventNameTCPDrop, uprobeFD, maxActive)
 	if err != nil {
 		panic(err)
 	}
